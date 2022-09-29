825fa4b6dfb3c325c5c15206f4ecec08dcb18add
Deepak Sreekumar <deepakstark@gmail.com>
2022-09-29 21:46:09 +0530

    feat(logger): aded support to move additional fields to metadata (#9)
    * feat(logger): aded support to move additional fields to metadata
    
    * feat(logger): update comment
    
    * feat(logger): remove unintentional change
    
    * fix(logger): added message field to root level
    
    Co-authored-by: DEEPAK S <deepak.s@safe.security>

--- src/safe_security_logger/logger.py
+++ src/safe_security_logger/logger.py
@@ -8,11 +8,13 @@ __author__ = "deepak.s@safe.security"
 __copyright__ = "Safe Security"
 __license__ = "MIT"
 
+ROOT_LEVEL_FIELDS = ["level", "service", "timestamp", "type", "message"]
+
 # Adding custom logger to support additional default field such as serviceName
 class CustomJsonFormatter(jsonlogger.JsonFormatter):
     def __init__(self, *args, **kwargs):
-        self.service = kwargs.pop("service", None)
         self.additional_fields = kwargs.pop("additional_fields", None)
+        self.root_level_fields = kwargs.pop("root_level_fields", None)
         super().__init__(*args, **kwargs)
 
     def add_fields(self, log_record, record, message_dict):
@@ -21,15 +23,31 @@ class CustomJsonFormatter(jsonlogger.JsonFormatter):
             if (not log_record.get(key)) and value:
                 log_record[key] = value
 
+        # all fields other than the root level fields would be moved to the metadata field to maintain consistency with NPM package
+        metadata = {}
+        log_record_copy = log_record.copy()
+        for key, value in log_record_copy.items():
+            if key not in self.root_level_fields:
+                metadata.update({key: value})
+                log_record.pop(key)
+        log_record["metadata"] = metadata
+
 
 def getLogger(
-    name, service=None, level=logging.INFO, handlers=[], additional_fields={}
+    name,
+    service=None,
+    level=logging.INFO,
+    handlers=[],
+    additional_fields={},
+    root_level_fields=ROOT_LEVEL_FIELDS,
 ):
 
     logger = logging.getLogger(name)
 
     if service:
         additional_fields.update({"service": service})
+
+    additional_fields.update({"type": "application"})
 
     formatter = CustomJsonFormatter(
         "%(asctime)s %(levelname)s %(message)s %(name)s %(module)s %(funcName)s %(lineno)d",
@@ -39,8 +57,8 @@ def getLogger(
             "funcName": "functionName",
             "name": "loggerName",
         },
-        service=service,
         additional_fields=additional_fields,
+        root_level_fields=root_level_fields,
     )
     # Use UTC time
     formatter.converter = time.gmtime
